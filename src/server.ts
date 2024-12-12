a6df87d18b3bf9f4f82296e5279cb97c10e2d540
Ilansuli1 <ilansuli2148@gmail.com>
2024-12-12 02:17:40 +0200

    deploy

--- src/server.ts
+++ src/server.ts
@@ -16,10 +16,10 @@ dotenvConfig();
 setupSocketAPI(http);
 
 const corsOptions = {
-  origin:
-    process.env.NODE_ENV === "production"
-      ? "https://elite-code.onrender.com"
-      : ["http://127.0.0.1:5173", "http://localhost:5173"],
+  origin: "http://localhost:5173",
+  // process.env.NODE_ENV === "production"
+  //   ? "https://elite-code.onrender.com"
+  //   : ["http://127.0.0.1:5173", "http://localhost:5173"],
   credentials: true,
 };
 
